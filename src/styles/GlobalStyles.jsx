d1db78d58a8083bac666eab0b45c234662f6e0ab
ZBrian99 <brianzelada1999@gmail.com>
2024-03-31 00:06:51 -0300

    setup

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+@import url("https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Cutive+Mono&family=Dancing+Script:wght@400..700&family=Poiret+One&display=swap");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+	fonts: {
+		primary: 'Cinzel, serif',
+		secondary: 'Cutive Mono, monospace',
+		tertiary: 'Dancing Script, cursive',
+		quaternary: 'Poiret One, cursive',
+	},
+	colors: {
+		primary: '#007bff', // Azul
+		secondary: '#28a745', // Verde
+		tertiary: '#dc3545', // Rojo
+		quaternary: '#ffc107', // Amarillo
+		quinary: '#17a2b8', // Cyan
+		senary: '#6610f2', // Púrpura
+	},
+};
