1d62583dbd23d4f02e02640b16f4b969d791d02d
KokotheKat <aglusa2020@gmail.com>
2024-03-31 02:25:36 -0300

    primerstaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { GlobalStyles } from "./styles/GlobalStyles"
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme.js';
+import {theme} from './styles/theme.js';
+import { ImageOverlay } from "./components/visuals/ImageOverlay.jsx";
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div>Hola</div>
+      <div><ImageOverlay imagen="https://images.unsplash.com/photo-1520854221256-17451cc331bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2VkZGluZ3xlbnwwfDB8MHx8fDA%3D"/></div>
     </ThemeProvider>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+<div>
+  <ul>
+    <li>Lorem, ipsum dolor.1</li>
+    <li>Lorem, ipsum dolor.2</li>
+    <li>Lorem, ipsum dolor.3</li>
+  </ul>
+  <ul>
+    <li>Lorem, ipsum dolor.4</li>
+    <li>Lorem, ipsum dolor.5</li>
+  </ul>
+</div>
+  );
+};
