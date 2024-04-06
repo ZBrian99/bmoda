dc0d0e77c0be56e336851c5ec9f867dab571d62d
ZBrian99 <brianzelada1999@gmail.com>
2024-04-06 00:56:16 -0300

    ensalada 2

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -1,31 +1,191 @@
-import { useRef } from 'react';
-import { motion, useTransform, useScroll } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import { motion, useTransform, useScroll, useSpring } from 'framer-motion';
 
-import '../styles/test.scss'
-
+import '../styles/test.scss';
+const platos = [
+	{
+		id: 1,
+		name: 'Pizza',
+		price: 20,
+		img: 'src/assets/images/pizza.jpg',
+	},
+	{
+		id: 2,
+		name: 'Hamburgesa',
+		price: 15,
+		img: 'src/assets/images/hamburgesa.jpg',
+	},
+	{
+		id: 3,
+		name: 'Sushi',
+		price: 25,
+		img: 'src/assets/images/sushi.jpg',
+	},
+	{
+		id: 4,
+		name: 'Carne',
+		price: 28,
+		img: 'src/assets/images/carne.jpg',
+	},
+	{
+		id: 5,
+		name: 'Ensalada',
+		price: 12,
+		img: 'src/assets/images/ensalada.jpg',
+	},
+	// {
+	// 	id: 6,
+	// 	name: 'Sushi',
+	// 	price: 25,
+	// 	img: 'src/assets/images/sushi.jpg',
+	// },
+	// {
+	// 	id: 7,
+	// 	name: 'Pizza',
+	// 	price: 20,
+	// 	img: 'src/assets/images/pizza.jpg',
+	// },
+	// {
+	// 	id: 8,
+	// 	name: 'Hamburgesa',
+	// 	price: 15,
+	// 	img: 'src/assets/images/hamburgesa.jpg',
+	// },
+	// {
+	// 	id: 9,
+	// 	name: 'Sushi',
+	// 	price: 25,
+	// 	img: 'src/assets/images/sushi.jpg',
+	// },
+	// {
+	// 	id: 10,
+	// 	name: 'Carne',
+	// 	price: 28,
+	// 	img: 'src/assets/images/carne.jpg',
+	// },
+	// {
+	// 	id: 11,
+	// 	name: 'Ensalada',
+	// 	price: 12,
+	// 	img: 'src/assets/images/ensalada.jpg',
+	// },
+	{
+		id: 12,
+		name: 'Sushi',
+		price: 2225,
+		img: 'src/assets/images/sushi.jpg',
+	},
+];
 export const HorizontalScroll = () => {
-   const targetRef = useRef(null);
+	const targetRef = useRef(null);
+	// const [transformValue, setTransformValue] = useState(0);
+	const { scrollYProgress } = useScroll({
+		target: targetRef,
+	});
+	// const windowOffset = -500 + 1;
+	// const platosOffset = 12 - 1;
+	const windowOffset = -innerWidth + 1;
+	const platosOffset = platos.length - 1;
+	const totalWidth = windowOffset * platosOffset;
 
-		const { scrollYProgress } = useScroll({
-			target: targetRef,
-		});
+	// useEffect(() => {
+	// 	const handleScrol = () => {
+	// 		setTransformValue(scrollYProgress.current * totalWidth);
+	// 		console.log('update',);
+	// 	};
 
-		const x = useTransform(scrollYProgress, [0, 1], ['0%', '-100%']);
+	// 	window.addEventListener('scroll', handleScrol);
+	// 	return () => {
+	// 		window.removeEventListener('scroll', handleScrol);
+	// 	};
+	// }, []);
+	// const spring = useSpring(transformValue, {
+	// 	stiffness: 150,
+	// 	damping: 30,
+	// 	overshootClamping: true,
+	// });
+
+	// const scaleX = useSpring(scrollYProgress, {
+	// });
+	// const x = useTransform(scrollYProgress, [0, 1], [0, -window.innerWidth * 3]);
+
+	// const x = useTransform(scrollYProgress, [0, 1], [0, totalWidth]);
+	////////////////////
+	const x = useSpring(useTransform(scrollYProgress, [0, 1], [0, totalWidth]), {
+		stiffness: 150,
+		damping: 30,
+		overshootClamping: true,
+	});
+
+	/////////////////////
+	// const x = useSpring(useTransform(scrollYProgress, [0, 1], [0, windowOffset * platosOffset]), {
+	// 	stiffness: 150, // Ajusta la rigidez de la primavera
+	// 	damping: 30, // Ajusta la amortiguación de la primavera
+	// });
+	// console.log('recarga');
+	// const x = useTransform(scrollYProgress, [0, 1], [0, (-window.innerWidth + 1) * (platos.length - 1)]);
+
+	// useEffect(() => {
+	// 	const handleScrol = () => {
+	// 		console.log(`${x.current}px`);
+	// 	};
+
+	// 	window.addEventListener('scroll', handleScrol);
+	// 	return () => {
+	// 		window.removeEventListener('scroll', handleScrol);
+	// 	};
+	// }, []);
+	// useEffect(() => {
+	// 	const handleResize = () => {
+	// 		setInnerWidth(window.innerWidth);
+	// 		console.log('hola');
+	// 	};
+
+	// 	window.addEventListener('resize', handleResize);
+	// 	return () => {
+	// 		window.removeEventListener('resize', handleResize);
+	// 	};
+	// }, []);
 
 	return (
 		<div className='container'>
 			<div className='top'></div>
-			<section ref={targetRef} className='HorizontalScroll'>
+			<section
+				ref={targetRef}
+				className='HorizontalScroll'
+				style={{
+					height: ` calc((100vw - 17px) * ${platos.length - 1})`,
+				}}
+			>
 				<div className='HorizontalScroll-Sticky'>
-					<motion.div style={{ x }} className='HorizontalScroll-Container '>
-						<div className='HorizontalScroll-Item'></div>
-						<div className='HorizontalScroll-Item'></div>
-						<div className='HorizontalScroll-Item'></div>
-						<div className='HorizontalScroll-Item'></div>
+          <motion.div
+            style={{x}}
+						// style={{ transform: `translateX(${x.current}px)` }}
+						// style={{ x: '-15015px' }}
+						// animate={{ x: `${transformValue}px` }}
+						// transition={{ duration: 0.5, type: 'spring' }}
+						className='HorizontalScroll-Container '
+					>
+						{platos.map((plato) => (
+							<Card key={plato.id} {...plato} />
+						))}
 					</motion.div>
 				</div>
 			</section>
 			<div className='bottom'></div>
 		</div>
+	);
+};
+
+const Card = ({ id, img, name, price }) => {
+	return (
+		<motion.div key={id} className='HorizontalScroll-Item'>
+			<img className='HorizontalScroll-ItemImage ' src={img} alt={name} />
+
+			<div className='HorizontalScroll-ItemInfo'>
+				<h3 className='HorizontalScroll-ItemTitle'>{name}</h3>
+				<p className='HorizontalScroll-ItemPrice '>${price}</p>
+			</div>
+		</motion.div>
 	);
 };
--- src/hooks/useScroll.jsx
+++ src/hooks/useScroll.jsx
@@ -1,45 +1,78 @@
-import { useState, useEffect, useRef } from 'react';
+// import { useState, useEffect, useRef } from 'react';
 
-const useScroll = (direction = 'vertical') => {
-	const [scrollPosition, setScrollPosition] = useState(0);
-	const [scrollDirection, setScrollDirection] = useState(null);
+// const useScroll = (direction = 'vertical') => {
+// 	const [scrollPosition, setScrollPosition] = useState(0);
+// 	const [scrollDirection, setScrollDirection] = useState(null);
+// 	const [scrollPercentage, setScrollPercentage] = useState(0);
+
+// 	const lastScrollPositionRef = useRef(0);
+
+// 	useEffect(() => {
+// 		const handleScroll = () => {
+// 			let currentScrollPosition;
+// 			if (direction === 'vertical') {
+// 				currentScrollPosition = window.scrollY;
+// 			} else if (direction === 'horizontal') {
+// 				currentScrollPosition = window.scrollX;
+// 			}
+
+// 			const scrollDir = currentScrollPosition > lastScrollPositionRef.current ? 'down' : 'up';
+// 			const windowSize = direction === 'vertical' ? window.innerHeight : window.innerWidth;
+// 			const fullSize = direction === 'vertical' ? document.body.clientHeight : document.body.clientWidth;
+// 			const scrollPercent = (currentScrollPosition / (fullSize - windowSize)) * 100;
+
+// 			setScrollPosition(currentScrollPosition);
+// 			setScrollDirection(scrollDir);
+// 			setScrollPercentage(scrollPercent);
+
+// 			lastScrollPositionRef.current = currentScrollPosition;
+// 		};
+
+// 		window.addEventListener('scroll', handleScroll);
+
+// 		return () => {
+// 			window.removeEventListener('scroll', handleScroll);
+// 		};
+// 	}, [direction]);
+
+// 	return {
+// 		scrollPosition,
+// 		scrollDirection,
+// 		scrollPercentage,
+// 	};
+// };
+
+// export default useScroll;
+
+
+
+import { useRef, useState, useEffect } from 'react';
+
+const useScroll = (ref) => {
 	const [scrollPercentage, setScrollPercentage] = useState(0);
 
-	const lastScrollPositionRef = useRef(0);
-
 	useEffect(() => {
-		const handleScroll = () => {
-			let currentScrollPosition;
-			if (direction === 'vertical') {
-				currentScrollPosition = window.scrollY;
-			} else if (direction === 'horizontal') {
-				currentScrollPosition = window.scrollX;
-			}
+		const scrollHandler = () => {
+			if (!ref.current) return; // Verificar si la referencia es null
 
-			const scrollDir = currentScrollPosition > lastScrollPositionRef.current ? 'down' : 'up';
-			const windowSize = direction === 'vertical' ? window.innerHeight : window.innerWidth;
-			const fullSize = direction === 'vertical' ? document.body.clientHeight : document.body.clientWidth;
-			const scrollPercent = (currentScrollPosition / (fullSize - windowSize)) * 100;
-
-			setScrollPosition(currentScrollPosition);
-			setScrollDirection(scrollDir);
-			setScrollPercentage(scrollPercent);
-
-			lastScrollPositionRef.current = currentScrollPosition;
+			const { scrollTop, scrollHeight, clientHeight } = ref.current;
+			const scrollableHeight = scrollHeight - clientHeight;
+			const percentage = scrollTop / scrollableHeight;
+			setScrollPercentage(percentage);
 		};
 
-		window.addEventListener('scroll', handleScroll);
+		if (ref.current) {
+			ref.current.addEventListener('scroll', scrollHandler);
+		}
 
 		return () => {
-			window.removeEventListener('scroll', handleScroll);
+			if (ref.current) {
+				ref.current.removeEventListener('scroll', scrollHandler);
+			}
 		};
-	}, [direction]);
+	}, [ref]);
 
-	return {
-		scrollPosition,
-		scrollDirection,
-		scrollPercentage,
-	};
+	return scrollPercentage;
 };
 
 export default useScroll;
